7d3c4d717f11afa000cc84f98f613b5ef7629779
Kevin Ren <kevinren1108@gmail.com>
2022-01-11 14:02:15 -0600

    add footer

---
+++ src/common/footer/index.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+class Footer extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <div>Footeer  </div>
+     );
+  }
+}
+ 
+export default Footer;
